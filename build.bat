85f3b8fa83a0ff8d0e8cd5c278b61eeef3062ba6
veenusav <veenusav@gmail.com>
2026-01-18 22:06:46 +0530

    gitignore added. build.bat is having temp and bin directories.

--- build.bat
+++ build.bat
@@ -1,4 +1,14 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 if %errorlevel% neq 0 exit /b %errorlevel%
-cl.exe /EHsc /Zi /std:c++17 /Fe:gitcopy.exe gitcopy.cpp
+setlocal enabledelayedexpansion
+set "config=%1"
+if "!config!"=="" set "config=debug"
+
+set "temp_dir=temp"
+set "bin_dir=bin\!config!"
+
+if not exist "!temp_dir!" mkdir "!temp_dir!"
+if not exist "!bin_dir!" mkdir "!bin_dir!"
+
+cl.exe /EHsc /Zi /std:c++17 /Fo"!temp_dir!\\" /Fd"!temp_dir!\gitcopy.pdb" /Fe"!bin_dir!\gitcopy.exe" gitcopy.cpp
